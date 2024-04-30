08c09bf894e8358b5fe17144eb0b63252e7d6894
hafane <hafanepwnz@yahoo.com>
2024-05-01 02:55:50 +0300

    Removed ADMIN role check for add rating and basket Item

--- server/route/basketItemRouter.js
+++ server/route/basketItemRouter.js
@@ -3,8 +3,8 @@ const router = new Router()
 const basketItemController = require('../controllers/basketItemController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRole(('ADMIN') || ('USER')), basketItemController.create)
-router.get('/',  checkRole(('ADMIN') || ('USER')), basketItemController.getAll)
-router.delete('/', checkRole(('ADMIN') || ('USER')), basketItemController.delete)
+router.post('/', checkRole('USER'), basketItemController.create)
+router.get('/',  checkRole('USER'), basketItemController.getAll)
+router.delete('/', checkRole('USER'), basketItemController.delete)
 
 module.exports = router
--- server/route/ratingsRouter.js
+++ server/route/ratingsRouter.js
@@ -3,8 +3,8 @@ const router = new Router()
 const ratingController = require('../controllers/ratingsController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRole(('ADMIN') || ('USER')), ratingController.create)
+router.post('/', checkRole('USER'), ratingController.create)
 router.get('/', ratingController.getAll)
-router.delete('/', checkRole(('ADMIN') || ('USER')), ratingController.delete)
+router.delete('/', checkRole('USER'), ratingController.delete)
 
 module.exports = router
