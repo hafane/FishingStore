2639501e66a8db1e9bc7cbcfa01f9297174be4d5
hafane <hafanepwnz@yahoo.com>
2024-05-03 02:12:08 +0300

    Added check for multiple roles

--- server/middleware/checkRoleMiddleware.js
+++ server/middleware/checkRoleMiddleware.js
@@ -11,7 +11,7 @@ module.exports = function(role) {
                 return res.status(401).json({message: 'Пользователь не авторизован'})
             }
             const decode = jwt.verify(token, process.env.SECRET_KEY)
-            if(decode.role !== role) {
+            if(!role.includes(decode.role)) {
                 return res.status(403).json({message: 'Нет доступа'})
             }
             req.user = decode
--- server/route/basketItemRouter.js
+++ server/route/basketItemRouter.js
@@ -3,9 +3,9 @@ const router = new Router()
 const basketItemController = require('../controllers/basketItemController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRole('USER'), basketItemController.create)
-router.get('/',  checkRole('USER'), basketItemController.getAll)
-router.delete('/', checkRole('USER'), basketItemController.delete)
-router.delete('/all', checkRole( 'ADMIN'), basketItemController.deleteItemId)
+router.post('/', checkRole(['USER', 'ADMIN']), basketItemController.create)
+router.get('/',  checkRole(['USER', 'ADMIN']), basketItemController.getAll)
+router.delete('/', checkRole(['USER', 'ADMIN']), basketItemController.delete)
+router.delete('/all', checkRole('ADMIN'), basketItemController.deleteItemId)
 
 module.exports = router
